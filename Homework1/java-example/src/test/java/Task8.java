3aeeeec27f103de4ed1eb08fc8ab157013710ce4
umka123 <37208372+umka123@users.noreply.github.com>
2018-03-21 10:36:02 +0500

    Task8 done

---
+++ Homework1/java-example/src/test/java/Task8.java
@@ -0,0 +1,41 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class Task8 {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver,10);
+    }
+
+    @Test
+    public void Test1() {
+        /* Start page */
+        driver.get("http://localhost/litecart");
+
+        /* Get the list of all items */
+        List<WebElement> duck;
+        duck = driver.findElements(By.cssSelector("div.image-wrapper"));
+        for (WebElement i : duck) {
+            i.findElement(By.cssSelector("div.sticker"));
+        }
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+
+}
